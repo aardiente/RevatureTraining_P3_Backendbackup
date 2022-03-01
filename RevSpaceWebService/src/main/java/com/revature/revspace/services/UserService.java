59dd59a535563ca5f4fd7d29ef982bdf46f4f46f
steven_hansen <srhansen849@gmail.com>
2022-03-01 16:36:18 -0500

    Mar 1

--- RevSpaceWebService/src/main/java/com/revature/revspace/services/UserService.java
+++ RevSpaceWebService/src/main/java/com/revature/revspace/services/UserService.java
@@ -6,6 +6,7 @@ import com.revature.revspace.repositories.UserRepo;
 public interface UserService extends CrudService<User, Integer, UserRepo>
 {
 	public User getUserByEmail(String email);
+	public User getUserByName(String firstname, String lastname);
 
 	/**
 	 * @return Currently logged-in user. Returns null if used outside an HTTP request scope.
--- RevSpaceWebService/src/main/java/com/revature/revspace/services/UserServiceImpl.java
+++ RevSpaceWebService/src/main/java/com/revature/revspace/services/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.revature.revspace.services;
 import com.revature.revspace.models.User;
 import com.revature.revspace.repositories.UserRepo;
 import com.revature.revspace.utils.LoggedInUser;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -41,4 +44,18 @@ public class UserServiceImpl implements UserService
     {
         return this.loggedInUser.getUser();
     }
+
+	@Override
+	public User getUserByName(String firstName, String lastName) {
+		List<User> firstname = ur.findByFirstName(firstName);
+		String name = firstName+lastName;
+		
+		for(User temp: firstname) {
+			String search = temp.getFirstName()+temp.getLastName();
+			if(search.equals(name))
+				return temp;
+		}
+		
+		return null;
+	}
 }
