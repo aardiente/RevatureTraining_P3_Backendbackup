3059cb01ec6cec2e947ee863f29da874af0df0db
Alexander Colin Starr <46503375+AStarr649@users.noreply.github.com>
2022-03-04 13:18:19 -0600

    Fixed Serialization conflict

--- RevSpaceWebService/src/main/java/com/revature/revspace/util/UserSerializer.java
+++ RevSpaceWebService/src/main/java/com/revature/revspace/util/UserSerializer.java
@@ -10,13 +10,13 @@ import com.revature.revspace.models.User;
 
 public class UserSerializer extends StdSerializer<User> {
     
-    public UserSerializer() {
-        this(null);
+    protected UserSerializer() {
+        super(User.class);
     }
-  
-    public UserSerializer(Class<User> t) {
-        super(t);
-    }
+//  
+//    public UserSerializer(Class<User> t) {
+//        super(t);
+//    }
 
 
 	@Override
@@ -26,9 +26,14 @@ public class UserSerializer extends StdSerializer<User> {
 		gen.writeNumberField("userId", value.getUserId());
 		gen.writeStringField("email", value.getEmail());
 		gen.writeStringField("firstName", value.getFirstName());
-		gen.writeStringField("lasstName", value.getLastName());
-		gen.writeNumberField("birthday", value.getBirthday());
-		gen.writeNumberField("revatureJoinDate", value.getRevatureJoinDate());
+		gen.writeStringField("lastName", value.getLastName());
+		if(value.getBirthday() != null) {
+			gen.writeNumberField("birthday", value.getBirthday());
+		}		
+		if(value.getRevatureJoinDate() != null) {
+			gen.writeNumberField("revatureJoinDate", value.getRevatureJoinDate());
+		}
+		//gen.writeNumberField("revatureJoinDate", value.getRevatureJoinDate());
 		gen.writeStringField("githubUsername", value.getGithubUsername());
 		gen.writeStringField("title", value.getTitle());
 		gen.writeStringField("location", value.getLocation());
@@ -38,8 +43,8 @@ public class UserSerializer extends StdSerializer<User> {
 			for(User field : value.getFollowers()) {
 				gen.writeStartObject();
 				gen.writeNumberField("userId", field.getUserId());
-				gen.writeStringField("firstName", value.getFirstName());
-				gen.writeStringField("lasstName", value.getLastName());
+				gen.writeStringField("firstName", field.getFirstName());
+				gen.writeStringField("lastName", field.getLastName());
 				gen.writeEndObject();
 			}
 		}
@@ -49,8 +54,8 @@ public class UserSerializer extends StdSerializer<User> {
 			for(User field : value.getFollowing()) {
 				gen.writeStartObject();
 				gen.writeNumberField("userId", field.getUserId());
-				gen.writeStringField("firstName", value.getFirstName());
-				gen.writeStringField("lasstName", value.getLastName());
+				gen.writeStringField("firstName", field.getFirstName());
+				gen.writeStringField("lastName", field.getLastName());
 				gen.writeEndObject();
 			}
 		}
